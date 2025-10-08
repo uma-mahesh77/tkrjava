01a03da0996ff8a77c14d1048a516f51cafd9c35
uma-mahesh77 <umamaheshannarapu@gmail.com>
2025-10-08 19:50:04 +0530

    inhertance

--- Tnsif/src/Inheritance/MultilevelInheritance.java
+++ Tnsif/src/Inheritance/MultilevelInheritance.java
@@ -1,28 +1,29 @@
 package Inheritance;
-class Animal {
+class Animals1 {
     void eat() {
         System.out.println("I can eat.");
     }
 }
 
-class Dog extends Animal {
+class Dogs extends Animals1 {
     void bark() {
         System.out.println("I can bark.");
     }
 }
 
-class Puppy extends Dog {
+class Puppy extends Dogs {
     void weep() {
         System.out.println("I am a puppy, I can weep.");
     }
 }
+
 public class MultilevelInheritance {
-	 public static void main(String[] args) {
-	        Puppy p = new Puppy();
-	        p.eat();   
-	        p.bark();  
-	        p.weep();  
-	    }
-	}
+    public static void main(String[] args) {
+        Puppy p = new Puppy();
+        p.eat();   
+        p.bark();  
+        p.weep();  
+    }
+}
 
 
--- Tnsif/src/Inheritance/SingleInheritance.java
+++ Tnsif/src/Inheritance/SingleInheritance.java
@@ -1,21 +1,20 @@
 package Inheritance;
-class Animal {
+class Animal2 {
     void eat() {
         System.out.println("I can eat.");
     }
 }
 
-class Dog extends Animal {
+class Dog1 extends Animal2 {
     void bark() {
         System.out.println("I can bark.");
     }
 }
-public class SingleInheritance {
 
-	public static void main(String[] args) {
-		Dog d = new Dog();
+public class SingleInheritance {
+    public static void main(String[] args) {
+        Dog1 d = new Dog1();
         d.eat();  // from Animal
         d.bark(); // from Dog
     }
 }
-
